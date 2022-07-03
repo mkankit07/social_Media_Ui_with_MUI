2c4c4a10d28cb30539365fbad7914c7629fe493c
mkankit07 <mkankit189@gmail.com>
2022-07-03 12:59:00 +0530

    initial commit

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,12 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import { Post } from './Post'
-import Reel from './Reel'
+// import Reel from './Reel'
 
 export const Feed = () => {
   return (
     <Box padding={2} flex={4}>
-      <Reel/>
+      {/* <Reel/>s */}
       <Post/>
       <Post/>
       <Post/> 
